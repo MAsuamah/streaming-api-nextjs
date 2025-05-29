646150db3c9cf8c169dc6fb164f6b328e5ebf598
Michelle Asuamah <michelleasuamah@Michelles-MBP-2.lan>
2025-05-29 14:00:09 -0400

    add env file

--- src/app/api/token/route.js
+++ src/app/api/token/route.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
+import 'dotenv/config'
+
 
 export async function GET() {
   try {
     const response = await axios.post(
       'https://api.assemblyai.com/v2/realtime/token',
-      { expires_in: 3600 },
-      { headers: { authorization: "<YOUR_API_KEY>" } }
+      { expires_in: 60 },
+      { headers: { authorization: process.env.ASSEMBLYAI_API_KEY} }
     );
 
     return Response.json(response.data, { status: 200 });
